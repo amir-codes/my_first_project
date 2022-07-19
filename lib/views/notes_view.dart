cf716c026c86c2b43709c9dde1eece933e5e6d2a
lord-sifo <106556117+lord-sifo@users.noreply.github.com>
2022-07-19 01:29:45 +0100

    step-9

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_my_notes/constants/routes.dart';
+import 'package:flutter_my_notes/enum/menu_action.dart';
+import 'package:flutter_my_notes/services/auth/auth_service.dart';
+
+class NoteView extends StatefulWidget {
+  const NoteView({Key? key}) : super(key: key);
+
+  @override
+  State<NoteView> createState() => _NoteViewState();
+}
+
+class _NoteViewState extends State<NoteView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Main UI'),
+        actions: [
+          PopupMenuButton<MenuAction>(
+            onSelected: (value) async {
+              switch (value) {
+                case MenuAction.logout:
+                  final shouldLogout = await showLogOutDialog(context);
+                  if (shouldLogout) {
+                    await AuthService.firebase().logOut();
+                    Navigator.of(context)
+                        .pushNamedAndRemoveUntil(loginRoute, (_) => false);
+                  }
+              }
+            },
+            itemBuilder: (context) {
+              return [
+                const PopupMenuItem<MenuAction>(
+                  value: MenuAction.logout,
+                  child: Text('logout'),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: const Text('Hello World'),
+    );
+  }
+}
+
+Future<bool> showLogOutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Sign out'),
+          content: const Text('Are you sure you want to sign out?'),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text('Cancel')),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text('Log out')),
+          ],
+        );
+      }).then((value) => value ?? false);
+}
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,6 +1,7 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_my_notes/constants/routes.dart';
+import 'package:flutter_my_notes/services/auth/auth_exceptions.dart';
+import 'package:flutter_my_notes/services/auth/auth_service.dart';
 import 'package:flutter_my_notes/utilities/show_error_dialog.dart';
 
 class RegisterView extends StatefulWidget {
@@ -56,40 +57,32 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                await AuthService.firebase().createUser(
                   email: email,
                   password: password,
                 );
-                final user = FirebaseAuth.instance.currentUser;
-                user?.sendEmailVerification();
+                AuthService.firebase().sendEmailVerification();
                 Navigator.of(context).pushNamed(verifyEmailRoute);
-              } on FirebaseAuthException catch (e) {
-                if (e.code == 'weak-password') {
-                  await showErrorDialog(
+              } on WeakPasswordAuthException {
+                await showErrorDialog(
                     context,
                     'Weak password',
                   );
-                } else if (e.code == 'email-already-in-use') {
-                  await showErrorDialog(
+              } on EmailAlreadyInUseAuthException{
+                await showErrorDialog(
                     context,
                     'Email already in use',
                   );
-                } else if (e.code == 'invalid-email') {
-                  await showErrorDialog(
+              } on InvalidEmailAuthException{
+                await showErrorDialog(
                     context,
                     'This is an invalid email adress',
                   );
-                } else {
-                  await showErrorDialog(
-                    context,
-                    'Error : ${e.code}',
-                  );
-                }
-              } catch (e) {
+              } on GenericAuthException{
                 await showErrorDialog(
-                  context,
-                  e.toString(),
-                );
+                    context,
+                    'failed to register',
+                  );
               }
             },
             child: const Text('Register'),
